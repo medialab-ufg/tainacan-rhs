952b0b1cedfa76799445ecc703244f68c3081557
Fabiano Alencar <fabianocarvalho15@gmail.com>
2017-12-04 16:09:45 -0200

    Tainacan Child RHS - Pesquisa a Busca avançada

--- searchform.php
+++ searchform.php
@@ -7,15 +7,23 @@
  * @since Rede Humaniza SuS
  */
 ?>
-<form autocomplete="off" class="form-search-rhs navbar-form navbar-left" role="search" action="<?php echo home_url(); ?>" method="get" id="menuPesquisa">
+<form autocomplete="off" class="form-search-rhs navbar-form navbar-left" role="search" id="formSearchCollections">
     <div class="form-group" style="display: inline;">
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="Digite aqui o que você procura." size="15" maxlength="128" name="s" id="search" value="">
+            <input type="text" class="form-control" placeholder="Pesquise em nosso acervo..." size="15" maxlength="128" name="search_collections" id="search_collections" value="">
             <span class="input-group-btn">
                 <button type="submit" class="btn btn-default">
                     <span class="glyphicon glyphicon-search"></span>
                 </button>
             </span>
+            <a onclick="redirectAdvancedSearch(false);" href="javascript:void(0)" class="adv_search">
+                <span class="white"><?php _e('Advanced search', 'tainacan') ?></span>
+            </a>
         </div>
     </div>
 </form>
+<script>
+    if($("#search_collections").is(":focus")){
+        $(".adv_search").css(display: 'block !important');
+    }
+</script>
