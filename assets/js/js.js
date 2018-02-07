3c5d221ae10e122ba55c95bea791ca67b3f2d2d6
Pedro Naves <oi@pedronaves.com.br>
2018-02-07 11:25:04 -0200

    removendo botões de compartilhamento quando capa não está ativa

--- assets/js/js.js
+++ assets/js/js.js
@@ -16,13 +16,17 @@ $(document).ready(function () {
     
 });
 
-function checkBreadcrumb () {
+function checkBreadcrumb() {
     var element_to_check = '#tainacan-breadcrumbs a';
     if($(element_to_check).is(':visible')){ 
         $(element_to_check).first().text('Acervo');
     } else {
         setTimeout(checkBreadcrumb, 50);
     }
+}
+
+function remove_share() {
+    $('.row .tainacan-museum-clear').hide();
 }
 
 function display_view_main_page() {
